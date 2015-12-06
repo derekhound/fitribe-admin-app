0025a87be8a3c690a2943b7d7bb3e3909a1273f0
derekhound <derekhound@gmail.com>
2015-12-06 18:45:21 +0800

    Add auth login feature

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -52,7 +52,8 @@ angular
         abstract: true,
         views: {
           'header': {
-            templateUrl: 'views/header.html'
+            templateUrl: 'views/header.html',
+            controller: 'HeaderCtrl'
           },
           'footer': {
             templateUrl: 'views/footer.html'
--- app/scripts/controllers/header-ctrl.js
+++ app/scripts/controllers/header-ctrl.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('app')
-  .controller('HeaderCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('HeaderCtrl', [
+    '$q', '$scope', 'AuthService', 'User',
+    function ($q, $scope, AuthService, User) {
+
+    // view model
+    $scope.vm = {};
+
+    $scope.isAuthenticated = function() {
+      return User.isAuthenticated();
+    };
+
+    $scope.logout = function() {
+      AuthService.logout();
+    };
+
+    function init() {
+
+    };
+
+    init();
+
+  }]);
